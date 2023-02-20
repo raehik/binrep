3517fa3e53423dbce93af5e14403425767ed3cb2
Ben Orchard <thefirstmuffinman@gmail.com>
2023-02-20 03:25:09 +0000

    update Nix flake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    #flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    flatparse = {
+      url = "github:AndrasKovacs/flatparse";
+      flake = false;
+    };
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, ... }: {
         haskellProjects.default = {
-          #haskellPackages = pkgs.haskell.packages.ghc925;
-          haskellPackages = pkgs.haskell.packages.ghc944;
-          packages = {
-            binrep.root = ./.;
+          # GHC 9.4 is bust on Nix, fix
+          basePackages = pkgs.haskell.packages.ghc94.override {
+            overrides = self: super: {
+              ormolu = self.ormolu_0_5_3_0;
+            };
           };
-          # nix hls, ghcid breaks on 9.4
-          buildTools = _: { haskell-language-server = null; ghcid = null; hlint
-        = null; };
-          overrides = self: super: {
-            hspec-smallcheck = super.hspec-smallcheck_0_5_3;
-            mason = super.mason_0_2_6;
-            flatparse = self.callCabal2nix "flatparse" (pkgs.fetchFromGitHub {
-              owner = "AndrasKovacs";
-              repo = "flatparse";
-              rev = "d96616b7abdf78f064f775c47d218c4721daa38c";
-              sha256 = "eO4L+QTsn+HbawjJO2j/EJag62OKMH/PzbcyZJKEbu0=";
-            }) {};
+          # packages.example.root = ./.;  # This value is detected based on .cabal files
+          overrides = self: super: with pkgs.haskell.lib; {
+            flatparse = self.callCabal2nix "flatparse" inputs.flatparse {};
           };
-          # hlintCheck.enable = true;
-          # hlsCheck.enable = true;
+          devShell = {
+            tools = hp: {
+              ghcid = null; # ghcid broken on Nix for GHC 9.4
+              haskell-language-server = null; # needs a massive system to build
+            };
+          };
         };
         # haskell-flake doesn't set the default package, but you can do it here.
-        #packages.default = self'.packages.my-package;
+        packages.default = self'.packages.binrep;
       };
     };
 }
