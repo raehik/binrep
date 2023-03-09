514d711d22d22469236bc7e11dc6152a248dd539
Ben Orchard <thefirstmuffinman@gmail.com>
2023-03-09 19:56:31 +0000

    update Nix flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,46 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    #flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
+
       perSystem = { self', pkgs, ... }: {
+
+        # Typically, you just want a single project named "default". But
+        # multiple projects are also possible, each using different GHC version.
         haskellProjects.default = {
-          #haskellPackages = pkgs.haskell.packages.ghc925;
-          packages = {
-            binrep.root = ./.;
-          };
-          # buildTools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-          buildTools = hp: { stack = hp.stack; };
-          # overrides = self: super: { }
-          # hlintCheck.enable = true;
-          # hlsCheck.enable = true;
+          # If you have a .cabal file in the root, this option is determined
+          # automatically. Otherwise, specify all your local packages here.
+          # packages.example.root = ./.;
+
+          # The base package set representing a specific GHC version.
+          # By default, this is pkgs.haskellPackages.
+          # You may also create your own. See https://haskell.flake.page/package-set
+          # basePackages = pkgs.haskellPackages;
+
+          # Dependency overrides go here. See https://haskell.flake.page/dependency
+          # source-overrides = { };
+          # overrides = self: super: { };
+
+          # devShell = {
+          #  # Enabled by default
+          #  enable = true;  
+          #
+          #  # Programs you want to make available in the shell.
+          #  # Default programs can be disabled by setting to 'null'
+          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+          #
+          #  hlsCheck.enable = true;
+          # };
         };
+
         # haskell-flake doesn't set the default package, but you can do it here.
-        #packages.default = self'.packages.my-package;
+        packages.default = self'.packages.binrep;
       };
     };
 }
