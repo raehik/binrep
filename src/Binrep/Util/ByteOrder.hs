fe8a343dc19b65ff51eefa0d52cf8c2c132c8629
Ben Orchard <thefirstmuffinman@gmail.com>
2024-03-17 14:49:52 +0000

    ByteOrder: missing strongweak instances

--- src/Binrep/Util/ByteOrder.hs
+++ src/Binrep/Util/ByteOrder.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Binrep.Util.ByteOrder
   ( ByteOrder(..), ByteOrdered(..)
   , type LE, type BE, type Endian
@@ -5,6 +7,11 @@ module Binrep.Util.ByteOrder
 
 import Raehik.Compat.Data.Primitive.Types.Endian ( ByteOrdered(..) )
 import GHC.ByteOrder ( ByteOrder(..) )
+import Strongweak
+import Data.Kind ( Type )
+
+deriving via (a :: Type) instance     Weaken a =>     Weaken (ByteOrdered end a)
+deriving via (a :: Type) instance Strengthen a => Strengthen (ByteOrdered end a)
 
 -- shorter names I originally used
 type LE = 'LittleEndian
